bb46c833fd7f016743e3e8547e4beacb9ac38887
volodia99 <gaylor.wafflard@univ-grenoble-alpes.fr>
2024-06-13 18:38:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  
-  experimental: {
-    appDir: false,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.dribbble.com',
-      },
-    ],
-  },
-}
+//   experimental: {
+//     appDir: false,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'cdn.dribbble.com',
+//       },
+//     ],
+//   },
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
