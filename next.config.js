6c1d15d08ea1931891841a1f6689d66fd166011d
volodia99 <gaylor.wafflard@univ-grenoble-alpes.fr>
2024-06-15 19:25:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,39 +1,39 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',  
-//   experimental: {
-//     appDir: false,
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'cdn.dribbble.com',
-//       },
-//     ],
-//   },
-// }
-
-// module.exports = nextConfig
-
-// next.config.js
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  // Find the repository name from GITHUB_REPOSITORY
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  
+  experimental: {
+    appDir: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.dribbble.com',
+      },
+    ],
+  },
 }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true, // Since GitHub Pages does not support Next.js optimized images
-  },
-};
+module.exports = nextConfig
+
+// // next.config.js
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+// let assetPrefix = '';
+// let basePath = '';
+
+// if (isGithubActions) {
+//   // Find the repository name from GITHUB_REPOSITORY
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// }
+
+// module.exports = {
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
+//   images: {
+//     unoptimized: true, // Since GitHub Pages does not support Next.js optimized images
+//   },
+// };
