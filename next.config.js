af6cceefb4bb5cb3b2e13ec1b52da632ceaa1cb7
volodia99 <gaylor.wafflard@univ-grenoble-alpes.fr>
2024-06-16 03:16:55 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,19 +5,11 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = '';
 let basePath = '';
 
-if (isGithubActions) {
-  assetPrefix = ".";
-  basePath = ".";
-}
-
 const nextConfig = {
   output: 'export',  
   experimental: {
     appDir: false,
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  trailingSlash: true,
   images: {
     unoptimized: true, // Only set to true for GitHub Actions
     remotePatterns: [
