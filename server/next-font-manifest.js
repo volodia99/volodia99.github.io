24d7838273e05d996c0910ad8cc19113eb709f77
volodia99 <volodia99@users.noreply.github.com>
2024-06-16 00:52:23 +0000

    deploy: d1f473094194fc5be34e4a5a6229dd901a62eafb

---
+++ server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{"/_app":["static/media/0484562807a97172-s.p.woff2","static/media/8888a3826f4a3af4-s.p.woff2","static/media/4c285fdca692ea22-s.p.woff2"]},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":true}';
